ab23cc30da12b3cae892698b7951ad2dd239df93
orikeo <asteriosod@gmail.com>
2026-02-27 18:49:24 +0200

    change port for deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { app } from './app';
 import './types/express';
 import 'dotenv/config';
 
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
